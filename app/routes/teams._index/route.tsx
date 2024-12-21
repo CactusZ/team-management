98ab054fe48b5ddf7c89ad32be212f1b10c1d5aa
Igor Kasevich <igor.kasevich@pm.me>
2024-12-21 21:12:41 +0000

    fix: root view header layout

--- app/routes/teams._index/route.tsx
+++ app/routes/teams._index/route.tsx
@@ -13,12 +13,12 @@ export default function Teams() {
   const teamsData = useLoaderData<typeof loader>();
   return (
     <div className="max-h-full flex flex-col">
-      <div className="w-full border-b-2 pb-4 p-16">
+      <div className="flex justify-between items-center w-full border-b-2 pb-4 p-16">
         <h1 className="text-2xl font-bold">Teams</h1>
         <Form method="put" action="/teams">
           <button
             name="addTeam"
-            className="p-2 mt-2 bg-sky-500 text-sky-950 rounded hover:bg-sky-700 text-slate-100"
+            className="p-2 bg-sky-500 text-sky-950 rounded hover:bg-sky-700 text-slate-100"
           >
             Add Team
           </button>
